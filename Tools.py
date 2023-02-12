d39952ad8eb3fdcbc2cab9f7d2d3ac3051b5867d
Tharmsy! <101517800+Tharmsy@users.noreply.github.com>
2023-02-12 06:58:34 +0000

    Updated kinda shit
    i removed account gen cus it was dog 
    
    added chat bot you can customize it if you want but its kinda dogshit

--- Tools.py
+++ Tools.py
@@ -1,9 +1,8 @@
 import random
 import os
 import webbrowser
-import json
-import requests
 
+# Define the Folder Maker tool
 def folder_maker_tool():
     num_folders = int(input("How many folders do you want to create? "))
     folder_name = input("Enter the name of the folder: ")
@@ -12,69 +11,45 @@ def folder_maker_tool():
         os.mkdir(os.path.join(folder_location, f"{folder_name}_{i+1}"))
         print(f"Created folder {folder_name}_{i+1} at {folder_location}.")
 
+# Define the Discord Promotion tool
 def discord_promotion_tool():
     discord_link = "https://discord.gg/B73FFJpTnV"
     webbrowser.open(discord_link)
 
+# Define the Random Name Picker tool
 def random_name_picker_tool():
     names = input("Enter a comma-separated list of names: ").split(",")
     print(f"The randomly selected name is: {random.choice(names)}")
 
+# Define the Random Quote Generator tool
 def random_quote_generator_tool():
     quotes = ["The best way to predict the future is to invent it.", "Success is not final, failure is not fatal: it is the courage to continue that counts.", "It does not matter how slowly you go as long as you do not stop.", "You miss 100% of the shots you don't take.", "Believe you can and you're halfway there."]
     print(f"Random quote: {random.choice(quotes)}")
 
-def accounts_tool():
-    num_alts = int(input("How many Minecraft alts do you want to generate? "))
-    output_file = input("Enter the output file name: ")
+# Define the Chat Bot tool
+def chat_bot_tool():
+    responses = ["Hello!", "How are you?", "I'm sorry, I don't understand.", "Goodbye!"]
+    for i in range(3):
+        message = input("You: ")
+        response = random.choice(responses)
+        print(f"Bot: {response}")
 
-    alts = []
-    for i in range(num_alts):
-
-        email = f"{i}_{random.randint(0, 10000)}@example.com"
-        password = f"password_{random.randint(0, 10000)}"
-
-
-        alt = f"{email}:{password}"
-        alts.append(alt)
-
-    for alt in alts:
-        email, password = alt.split(":")
-        response = requests.post(
-            "https://authserver.mojang.com/authenticate",
-            headers={"Content-Type": "application/json"},
-            data=json.dumps({
-                "agent": {
-                    "name": "Minecraft",
-                    "version": 1
-                },
-                "username": email,
-                "password": password,
-                "requestUser": True
-            })
-        )
-
-        if response.status_code == 200:
-            print(f"Alt '{alt}' is valid!")
-        else:
-            print(f"Alt '{alt}' is invalid.")
-
-    if output_file:
-        with open(output_file, "w") as file:
-            file.write("\n".join(alts))
-    print(f"Saved {len(alts)} Minecraft alts to {output_file}.")
-
+# Define the main function to run the script
 def main():
-    tools = ["Folder Maker", "Discord Promotion", "Random Name Picker", "Random Quote Generator", "Accounts"]
+    # Define the list of tools
+    tools = ["Folder Maker", "Discord Promotion", "Random Name Picker", "Random Quote Generator", "Chat Bot"]
 
     while True:
+        # Print the list of tools
         print("\033[92mRandom Tools:\033[0m")
         for i, tool in enumerate(tools):
             print(f"{i+1}. {tool}")
         print("\033[92mOpen:\033[0m")
 
+        # Prompt the user to select a tool
         tool_choice = int(input("Enter the number of the tool you want to use: "))
 
+        # Execute the selected tool
         if tool_choice == 1:
             folder_maker_tool()
         elif tool_choice == 2:
@@ -84,10 +59,11 @@ def main():
         elif tool_choice == 4:
             random_quote_generator_tool()
         elif tool_choice == 5:
-            accounts_tool()
+            chat_bot_tool()
         else:
             print("Sorry, that tool is not available.")
 
+        # Ask if the user wants to repeat the process
         repeat_choice = input("Do you want to do something again? (y/n): ")
         if repeat_choice.lower() != "y":
             break
